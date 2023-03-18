ec99cfbe95954fddf58af2085df7664751089163
joeljojo <joelpamphyl@gmail.com>
2023-03-18 18:56:08 +0300

    Added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./Routes/routes');
 const database = require('./Config/db');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 const port = 8000;
 app.listen(port, () => {
