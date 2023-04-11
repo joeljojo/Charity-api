11b8d09d11cc69480b5d08f5ca7584f68b206396
joeljojo <joelpamphyl@gmail.com>
2023-04-11 08:48:21 +0300

    Added Error Messages

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -19,37 +19,41 @@ const registerUser = async (req, res) => {
   const userID = uuidv4();
 
   // Do validation
-  await registerSchema.validateAsync(data);
-  // Register if user does not exist
   try {
-    client.query(
-      `Select * from users  where not exists (select * from users where email =$1)`,
-      [email],
-      (error, result) => {
-        if (error) throw error;
-        if (result.rows.length > 0) {
-          client.query(`Insert into users values($1,$2,$3,$4,$5,$6,$7,$8)`, [
-            userID,
-            firstName,
-            lastName,
-            email,
-            hash,
-            isAdmin,
-            isDonor,
-            isChildrensHome,
-          ]);
-          res.json({
-            status: true,
-            message: 'User Registered Successfully!',
-          });
-        } else {
-          res.json({ status: false, message: 'User Already exists' });
+    await registerSchema.validateAsync(data);
+    try {
+      client.query(
+        `Select * from users  where not exists (select * from users where email =$1)`,
+        [email],
+        (error, result) => {
+          if (error) throw error;
+          if (result.rows.length > 0) {
+            client.query(`Insert into users values($1,$2,$3,$4,$5,$6,$7,$8)`, [
+              userID,
+              firstName,
+              lastName,
+              email,
+              hash,
+              isAdmin,
+              isDonor,
+              isChildrensHome,
+            ]);
+            res.json({
+              status: true,
+              message: 'User Registered Successfully!',
+            });
+          } else {
+            res.json({ status: false, message: 'User Already exists' });
+          }
         }
-      }
-    );
+      );
+    } catch (error) {
+      console.log(error);
+    }
   } catch (error) {
-    console.log(error);
+    res.json({ message: error.details[0].message });
   }
+  // Register if user does not exist
 };
 
 // Login controller
@@ -57,49 +61,55 @@ const userLogin = async (req, res) => {
   const { email, password } = req.body;
   const data = req.body;
   // Do validation
-  await loginSchema.validateAsync(data);
-
   try {
-    // if email exists and compare passwords
-    client.query(
-      `Select * from users where email= $1`,
-      [email],
-      (error, result) => {
-        if (error) throw error;
-        // password comparison
-        if (result.rows.length > 0) {
-          const storedPassword = result.rows[0].password;
-          const bool = bcrypt.compareSync(password, storedPassword);
+    await loginSchema.validateAsync(data);
+    try {
+      // if email exists and compare passwords
+      client.query(
+        `Select * from users where email= $1`,
+        [email],
+        (error, result) => {
+          if (error) throw error;
+          // password comparison
+          if (result.rows.length > 0) {
+            const storedPassword = result.rows[0].password;
+            const bool = bcrypt.compareSync(password, storedPassword);
 
-          // if passwords do not match
-          if (bool === false) {
+            // if passwords do not match
+            if (bool === false) {
+              res.json({
+                status: false,
+                message: 'Invalid Username or Password!',
+              });
+            } else {
+              // if passwords match
+              res.json({
+                status: true,
+                user: {
+                  userID: result.rows[0].userid,
+                  firstName: result.rows[0].firstname,
+                  lastName: result.rows[0].lastname,
+                  isAdmin: result.rows[0].isadmin,
+                  isDonor: result.rows[0].isdonaor,
+                  isChildrensHome: result.rows[0].ischildrenshome,
+                },
+                message: 'Login Successfully',
+              });
+            }
+          } else {
+            // if email does not exist
             res.json({
               status: false,
-              message: 'Invalid Username or Password!',
-            });
-          } else {
-            // if passwords match
-            res.json({
-              status: true,
-              user: {
-                userID: result.rows[0].userid,
-                firstName: result.rows[0].firstname,
-                lastName: result.rows[0].lastname,
-                isAdmin: result.rows[0].isadmin,
-                isDonor: result.rows[0].isdonaor,
-                isChildrensHome: result.rows[0].ischildrenshome,
-              },
-              message: 'Login Successfully',
+              message: 'Invalid Username or password!',
             });
           }
-        } else {
-          // if email does not exist
-          res.json({ status: false, message: 'Invalid Username or password!' });
         }
-      }
-    );
+      );
+    } catch (error) {
+      console.log(error);
+    }
   } catch (error) {
-    console.log(error);
+    res.json({ message: error.details[0].message });
   }
 };
 
